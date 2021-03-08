c820f4c94061cc2d3f52e2517f18f54a0ddd7db2
yabuki <shiroro.96646@gmail.com>
2021-03-08 11:28:49 +0900

    sub7

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,6 +18,9 @@ python3 manage.py migrate
 # DJANGO_SUPERUSER_PASSWORD=$DJANGO_SUPERUSER_PASSWORD \
 # DJANGO_SUPERUSER_EMAIL=$DJANGO_SUPERUSER_EMAIL \
 # python3 manage.py createsuperuser --noinput
+cd wantem
+ls -l
+cd ../
 
 celery -A config worker --detach
 gunicorn config.wsgi --bind=0.0.0.0:8000
