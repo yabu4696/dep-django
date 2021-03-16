f94d9cf740dfc76c317bf7172617b1f1a761c37f
yabuki <shiroro.96646@gmail.com>
2021-03-16 15:37:24 +0900

    2021-03-16_11

---
+++ ca_camera/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'ca_camera'
+urlpatterns = [
+    path('',  views.index, name='index'),
+    path('item/<slug:slug>', views.detail, name='detail'),
+    path('delete', views.delete, name='delete'),
+    path('item/<slug:slug>/exclusion', views.exclusion, name='exclusion'),
+    path('maker_index',views.maker_index, name='maker_index'),
+    path('maker/<slug:slug>', views.maker_detail, name='maker_detail'),
+    path('contact', views.contact, name='contact'), 
+    path('contact/done', views.done, name='done'),
+    # path('rayout',views.rayout,name='rayout'),
+    # path('form', views.form, name='form'),
+    # path('reload/',views.reload, name='reload'),
+    # path('item/<slug:slug>/edit', views.edit,name='edit'),
+    # path('item/<slug:slug>/reload_one', views.reload_one, name='reload_one'),
+    # path('celery_test/', views.celery_test, name='celery_test'),
+]
