84e057efab9cfa65eafe10890b637130e2a3c1d8
yabuki <shiroro.96646@gmail.com>
2021-03-24 06:41:00 +0900

    2021-03-24_5

--- ca_camera/models.py
+++ ca_camera/models.py
@@ -5,7 +5,7 @@ from . import def_chrome
 class Item_maker(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(null=False, unique=True)
-    meta_des = models.CharField(default='<meta name="description" content="カメラのレビューや評価サイトだけを表示したいというお悩みはありませんか？本サイトでは、カメラのレビューや評価サイトサイトだけを一覧表示！あなたに最適なアイテムを見つけてください。">')
+    meta_des = models.CharField(max_length=255,default='<meta name="description" content="カメラのレビューや評価サイトだけを表示したいというお悩みはありませんか？本サイトでは、カメラのレビューや評価サイトサイトだけを一覧表示！あなたに最適なアイテムを見つけてください。">')
 
     def __str__(self):
         return self.name
