8ceeb31a3949558a7189c252299bfbe19bd0d80d
yabuki <shiroro.96646@gmail.com>
2021-03-24 06:20:36 +0900

    2021-03-24_3

--- ca_camera/admin.py
+++ ca_camera/admin.py
@@ -2,10 +2,6 @@ from django.contrib import admin
 
 from .models import Item_maker, Tag, Wantoitem
 
-class WantoitemAdmin(admin.ModelAdmin):
-    fields = ('item_name', 'maker_name', 'tag', 'created_at', 'updated_at', 'slug', 'meta_des')
-    readonly_fields = ('created_at', 'updated_at')
-
 admin.site.register(Item_maker)
 admin.site.register(Tag)
-admin.site.register(Wantoitem, WantoitemAdmin)
+admin.site.register(Wantoitem)
