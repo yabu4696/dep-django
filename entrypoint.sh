19d5c067d51f810082c7c7924e2e8bee0553402a
yabuki <shiroro.96646@gmail.com>
2021-03-25 08:06:09 +0900

    2021-03-25_3

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ then
     echo "PostgreSQL started"
 fi
 
-# python3 manage.py flush --no-input
+python3 manage.py flush --no-input
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py showmigrations
@@ -19,10 +19,10 @@ python3 manage.py migrate --fake admin zero
 python3 manage.py showmigrations
 python3 manage.py migrate --fake
 python3 manage.py showmigrations
-# DJANGO_SUPERUSER_USERNAME=$DJANGO_SUPERUSER_USERNAME \
-# DJANGO_SUPERUSER_PASSWORD=$DJANGO_SUPERUSER_PASSWORD \
-# DJANGO_SUPERUSER_EMAIL=$DJANGO_SUPERUSER_EMAIL \
-# python3 manage.py createsuperuser --noinput
+DJANGO_SUPERUSER_USERNAME=$DJANGO_SUPERUSER_USERNAME \
+DJANGO_SUPERUSER_PASSWORD=$DJANGO_SUPERUSER_PASSWORD \
+DJANGO_SUPERUSER_EMAIL=$DJANGO_SUPERUSER_EMAIL \
+python3 manage.py createsuperuser --noinput
 cd ca_camera
 ls -l
 cd pattern
